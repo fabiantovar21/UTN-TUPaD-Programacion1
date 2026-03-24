f08dc7bd3534f64b75b4d53fd992b7b3131f9f6f
fabiantovar21 <fabian.tovar2002@gmail.com>
2026-03-23 22:12:54 -0300

    Ingreso del achivo de actividad

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# UTN-TUPaD-Programacion1
+# repo\_tutorial
+
+
+
+cambio realizado en el README
+
+cambio realizado desde la nube
+
+Cambio realizado desde auxiliar
+
+Este es mi practica de la unidad 1
+
---
+++ main.py
@@ -0,0 +1,10 @@
+print('Hola mundo desde main!')
+
+nombre=input('Ingrese su nombre: ')
+
+print(f'hola {nombre}')
+
+numero=int(input('Ingrese un número: '))
+numero2=int(input('Ingrese otro número: '))
+
+print(f'la suma de los numeros es: {numero + numero2}')
